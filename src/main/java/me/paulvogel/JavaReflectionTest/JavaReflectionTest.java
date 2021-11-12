0e8fb8670a7933044d9e4efb63466a8658041d35
Paul Vogel <pavog@users.noreply.github.com>
2021-11-12 10:50:20 +0100

    Update JavaReflectionTest.java

--- src/main/java/me/paulvogel/JavaReflectionTest/JavaReflectionTest.java
+++ src/main/java/me/paulvogel/JavaReflectionTest/JavaReflectionTest.java
@@ -13,7 +13,7 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.logging.*;
 
-public class Main
+public class JavaReflectionTest
 {
     public static void main(String[] args)
     {
